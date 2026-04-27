b4ee744c364e6f8926b771222f096c59d03b9ff8
rozyczko <piotr.rozyczko@gmail.com>
2026-04-27 11:10:59 +0200

    react to changed resolution % in the exp tab

--- tests/test_py_experiment.py
+++ tests/test_py_experiment.py
@@ -9,8 +9,10 @@ class StubModelsLogic:
         self.index = 0
         self.set_scaling_result = True
         self.set_background_result = True
+        self.set_resolution_result = True
         self.last_scaling = None
         self.last_background = None
+        self.last_resolution = None
 
     def set_scaling_at_current_index(self, value):
         self.last_scaling = value
@@ -19,6 +21,10 @@ class StubModelsLogic:
     def set_background_at_current_index(self, value):
         self.last_background = value
         return self.set_background_result
+
+    def set_resolution_at_current_index(self, value):
+        self.last_resolution = value
+        return self.set_resolution_result
 
 
 class StubProjectLogic:
@@ -65,17 +71,21 @@ def test_setters_emit_signals_only_when_logic_accepts_change(monkeypatch, qcore_
 
     experiment.setScaling(2.5)
     experiment.setBackground(0.1)
+    experiment.setResolution('3.3')
 
     assert experiment._model_logic.last_scaling == 2.5
     assert experiment._model_logic.last_background == 0.1
-    assert changed == {'experiment': 2, 'external': 2}
+    assert experiment._model_logic.last_resolution == '3.3'
+    assert changed == {'experiment': 3, 'external': 3}
 
     experiment._model_logic.set_scaling_result = False
     experiment._model_logic.set_background_result = False
+    experiment._model_logic.set_resolution_result = False
     experiment.setScaling(7.0)
     experiment.setBackground(5.0)
+    experiment.setResolution('4.4')
 
-    assert changed == {'experiment': 2, 'external': 2}
+    assert changed == {'experiment': 3, 'external': 3}
 
 
 def test_load_routes_single_vs_multi_dataset_paths(monkeypatch, qcore_application):
